30647a00630b2339b34ee81ca5fa96fedfff26e7
Ddommm24 <dominicjjoel@hotmail.co.uk>
2025-11-15 13:07:35 +0000

    Initial

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseMovement : MonoBehaviour
+{
+    [Header("Settings")]
+    public float mouseSensitivity = 0.002f;
+
+    private float xRotation = 0f;
+    private float yRotation = 0f;
+    private Vector2 lookInput;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Apply the input to camera rotation
+        float mouseX = lookInput.x * mouseSensitivity;
+        float mouseY = lookInput.y * mouseSensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        yRotation += mouseX;
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+
+    // This must be public and take exactly one InputAction.CallbackContext parameter
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cameraTransform;
+
+    public float speed = 10f;
+    public float runSpeed = 20f;
+    public float gravity = -9.81f * 2;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+    private Vector2 moveInput;
+    private bool jumpPressed;
+    private bool isRunning;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //checking if we hit the ground to reset our falling velocity, otherwise we will fall faster the next time
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float currentSpeed = isRunning ? runSpeed : speed;
+
+        // Get movement relative to camera
+        Vector3 camForward = cameraTransform.forward;
+        camForward.y = 0f;
+        camForward.Normalize();
+
+        Vector3 camRight = cameraTransform.right;
+        camRight.y = 0f;
+        camRight.Normalize();
+
+        Vector3 move = camRight * moveInput.x + camForward * moveInput.y;
+        controller.Move(move * currentSpeed * Time.deltaTime);
+
+        // Jump
+        if (jumpPressed && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            jumpPressed = false;
+        }
+
+        // Gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    // Called by Input System (PlayerInput component)
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            jumpPressed = true;
+            Debug.Log("Jump pressed!");
+        }
+    }
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            isRunning = true;
+            Debug.Log("Is running!");
+        }
+        else if (context.canceled)
+            isRunning = false;
+    }
+}
